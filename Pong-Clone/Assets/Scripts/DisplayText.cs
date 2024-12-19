87af088b1f8128001ab32bc009a0e10b58bb1daf
Joltfreak <joltfreak82@gmail.com>
2024-12-18 22:43:13 -0600

    Added text for scoring and winning
    Added text for when a player or AI scores, also added a end condition to when the game is over. Still need to set up a restart button when game ends

---
+++ Pong-Clone/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayText : MonoBehaviour
+{
+    TMP_Text playerScoredText;
+    TMP_Text aiScoredText;
+    ScoreHandling scoreHandling;
+
+    void Start()
+    {
+        scoreHandling = FindObjectOfType<ScoreHandling>();
+        playerScoredText = GameObject.Find("PlayerScoredText").GetComponent<TMP_Text>();
+        aiScoredText = GameObject.Find("AiScoredText").GetComponent<TMP_Text>();
+    }
+
+    public void DisplayEndGameText()
+    {
+        if(scoreHandling.player_score > scoreHandling.ai_score)
+        {
+            playerScoredText.text = "Player Wins!";
+        }
+        else
+        {
+            aiScoredText.text = "AI Wins!";
+        }
+    }
+
+    public IEnumerator DisplayPlayerScoredText()
+    {
+        playerScoredText.text = "Player Scored!";
+        yield return new WaitForSeconds(2);
+        playerScoredText.text = "";
+    }
+
+    public IEnumerator DisplayAiScoredText()
+    {
+        aiScoredText.text = "AI Scored!";
+        yield return new WaitForSeconds(2);
+        aiScoredText.text = "";
+    }
+}
--- Pong-Clone/Assets/Scripts/ScoreHandling.cs
+++ Pong-Clone/Assets/Scripts/ScoreHandling.cs
@@ -7,11 +7,13 @@ public class ScoreHandling : MonoBehaviour
     public int player_score = 0;
     public int ai_score = 0;
 
+    DisplayText displayText;
     SpawnBall spawnBall;
     BallMovement ballMovement;
 
     private void Start() 
     {
+        displayText = FindObjectOfType<DisplayText>();
         spawnBall = FindObjectOfType<SpawnBall>();
         ballMovement = FindObjectOfType<BallMovement>();
     }
@@ -28,13 +30,29 @@ public class ScoreHandling : MonoBehaviour
         {
             ai_score++;
             ballMovement.ballMoveSpeed = 5f;
-            spawnBall.StartCoroutine(spawnBall.spawnBall());
+            if(ai_score == 5 || player_score == 5)
+            {
+                displayText.DisplayEndGameText();
+            }
+            else
+            {
+                displayText.StartCoroutine(displayText.DisplayAiScoredText());
+                spawnBall.StartCoroutine(spawnBall.spawnBall());
+            }   
         }
         else if(other.gameObject.tag == "AiPointZone")
         {
             player_score++;
             ballMovement.ballMoveSpeed = 5f;
-            spawnBall.StartCoroutine(spawnBall.spawnBall());
+            if(ai_score == 5 || player_score == 5)
+            {
+                displayText.DisplayEndGameText();
+            }
+            else
+            {
+                displayText.StartCoroutine(displayText.DisplayPlayerScoredText());
+                spawnBall.StartCoroutine(spawnBall.spawnBall());
+            }   
         }
     }
 }
