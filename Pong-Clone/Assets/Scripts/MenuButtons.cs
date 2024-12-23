c3e86d9c03c4359a32d8325e9c2f3823effd0d8e
Joltfreak <joltfreak82@gmail.com>
2024-12-23 16:50:35 -0600

    Starting the buttons

--- Pong-Clone/Assets/Scripts/MenuButtons.cs
+++ Pong-Clone/Assets/Scripts/MenuButtons.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    // TODO: Make a sub-menu for game different game modes
+    public void DisplayGameModes()
     {
-        
+        // TODO: Display game modes
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BackToMainMenu()
     {
-        
+        // TODO: Make a method to go back to the main menu
+
+    }
+
+    public void StartGame(int gameMode)
+    {
+        // This will start the game
+        SceneManager.LoadScene(gameMode);
     }
 }
