890ac12c7c2cf7b4c14360c90a9afc21069ccfb4
Joltfreak <joltfreak82@gmail.com>
2024-12-29 21:34:54 -0600

    Finished Buttons
    Finished Functionality of Main Menu Buttons

--- Pong-Clone/Assets/Scripts/MenuButtons.cs
+++ Pong-Clone/Assets/Scripts/MenuButtons.cs
@@ -1,25 +1,44 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {   
+    GameObject gameModesDisplay;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip buttonClick;
+
+    private void Start()
+    {
+        gameModesDisplay = GameObject.Find("GameModesDisplay");
+        gameModesDisplay.SetActive(false);
+    }
+
     // TODO: Make a sub-menu for game different game modes
     public void DisplayGameModes()
     {
         // TODO: Display game modes
-
+        audioSource.PlayOneShot(buttonClick);
+        gameModesDisplay.SetActive(true);
     }
 
     public void BackToMainMenu()
     {
         // TODO: Make a method to go back to the main menu
-
+        audioSource.PlayOneShot(buttonClick);
+        gameModesDisplay.SetActive(false);
     }
 
-    public void StartGame(int gameMode)
+    public void StartPlayerVsAi(int gameMode)
     {
         // This will start the game
+        audioSource.PlayOneShot(buttonClick);
+        SceneManager.LoadScene(gameMode);
+    }
+
+    public void StartPlayerVsPlayer(int gameMode)
+    {
+        // This will start the game
+        audioSource.PlayOneShot(buttonClick);
         SceneManager.LoadScene(gameMode);
     }
 }
--- Pong-Clone/Assets/Scripts/PlayerInputHandling.cs
+++ Pong-Clone/Assets/Scripts/PlayerInputHandling.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInputHandling : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] bool isPlayerOne;
 
     // Update is called once per frame
     void Update()
     {
-        handlePlayerMovement();
+        if(isPlayerOne)
+        {
+            handlePlayer1Movement();
+        }
+        else
+        {
+            handlePlayer2Movement();
+        }
     }
 
-    void handlePlayerMovement()
+    void handlePlayer1Movement()
     {
         // range to clamp is 4.15 to -4.15
 
@@ -21,6 +27,20 @@ public class PlayerInputHandling : MonoBehaviour
             transform.position += Vector3.up * moveSpeed * Time.deltaTime;
         }
         else if(transform.position.y > -4.15f && Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void handlePlayer2Movement()
+    {
+        // range to clamp is 4.15 to -4.15
+
+        if(transform.position.y < 3.45f && Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+        else if(transform.position.y > -4.15f && Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down * moveSpeed * Time.deltaTime;
         }
